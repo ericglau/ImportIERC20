874e3b73f518d548392d543373e851a992fa6f8b
Eric Lau <ericglau@outlook.com>
2022-03-30 10:05:06 -0400

    testcase with erc20

--- contracts/ImportIERC20.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts-upgradeable/interfaces/IERC20Upgradeable.sol";
-
-contract ImportIERC20  {
-    
-}
--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,6 +1,7 @@
 const { deployProxy } = require("@openzeppelin/truffle-upgrades");
-const ImportIERC20 = artifacts.require("ImportIERC20");
-
+const ImportIERC20 = artifacts.require("MyToken");
 module.exports = async function (deployer) {
-  await deployProxy(ImportIERC20, { deployer });
+  const tok = await deployProxy(ImportIERC20, { deployer });
+  console.log(await tok.name());
+  //await deployer.deploy(ImportIERC20);
 };
